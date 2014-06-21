053033e0cd898961255e75997d4d91e97da572a7
Julia <juliajtan@gmail.com>
2014-06-21 14:01:11 +0100

    Added database_cleaner gem and configured RSpec to clean test database after each test run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,21 @@ ENV["RACK_ENV"] = "test" # sets the database to be used
 
 require './app/server' # note that this has to come after ENV is set!
 
+require 'database_cleaner'
+
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 end
