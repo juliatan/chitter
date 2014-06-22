7aebbe66fbafbbf9bd49206891ca7a9f6d46b7a4
Julia <juliajtan@gmail.com>
2014-06-22 02:35:40 +0100

    Maker must be signed in before they can make a peep

--- spec/features/adding_new_peeps_spec.rb
+++ spec/features/adding_new_peeps_spec.rb
@@ -2,9 +2,24 @@ require 'spec_helper'
 
 feature 'Maker submits a new peep'do
   
-  scenario "when browsing the website" do
+  before(:each) {
+    Maker.create(:email => "test@test.com",
+                  :name => "Julia",
+                  :username => "test_handle",
+                  :password => "test",
+                  :password_confirmation => "test")
+  }
+
+  scenario "must be signed in to be able to post a peep" do
+    expect(Peep.count).to eq 0
+    visit '/peeps/new'
+    expect(page).to have_content "You need to sign up for a Maker account before you can post a peep."
+  end
+
+  scenario "when logged in" do
     expect(Peep.count).to eq 0
     visit '/'
+    sign_in("test_handle", "test")
     add_peep("Hello world!")
     expect(Peep.count).to eq 1
     peep = Peep.first
